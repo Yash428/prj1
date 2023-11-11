9be0c704e1f7ff92d9f518df685859e82b2a8b83
Yash Patel <90138947+Yash428@users.noreply.github.com>
2023-11-11 17:42:45 +0530

    2nd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/*const ReactElement = {
+  type:'a',
+  props: {
+      href : 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me To visit google'
+}
+function MyApp(){
+  return(
+    <div></div>
+  )
+}*/
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
+const anotherElement = (
+  <a href='https://google.com' target= '_blank'>visit here</a>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
